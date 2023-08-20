b578f6e68bb2fb3d9dd0612ede961a11c903e8e5
Ayush Shyam Lingayat <lingayatayush3@gmail.com>
2023-08-20 11:32:07 +0530

    Initial commit

---
+++ color_switcher.js
@@ -0,0 +1,30 @@
+const violet = document.getElementById('violet');
+const indigo = document.getElementById('indigo');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+const yellow = document.getElementById('yellow');
+const orange = document.getElementById('orange');
+const red = document.getElementById('red');
+
+violet.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'violet'
+})
+indigo.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'indigo'
+})
+blue.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'blue'
+})
+green.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'green'
+})
+yellow.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'goldenrod'
+})
+orange.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'orange'
+})
+red.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'red'
+})
+
